1a73f9578bd7fe2059755a39dfe6ae78ac0fdaf9
tmhglnd <timohoogland@gmail.com>
2026-01-10 12:31:49 +0100

    docs for new objectmappings and mappings for jit.gl.pass

--- javascript/mapping-generator.js
+++ javascript/mapping-generator.js
@@ -1,11 +1,12 @@
 //
-// jit.gl.slab/shader shortcut generator
+// jit.gl.slab/shader/pix/pass shortcut generator
 //
-// Generates a mapping file for every jit.gl.slab/shader shipped
-// with the Max 8 Application
+// Generates a mapping file for every jit.gl.slab/pix/shader/pass shipped
+// with the Max 8/9 Application
 //
 // from gl.shadermappings (github.com/tmhglnd/gl.shadermappings)
-// written by Timo Hoogland, 2021, www.timohoogland.com
+// extended for AudioVisual Toolbox in 2023-2025
+// written by Timo Hoogland, 2021-2025, www.timohoogland.com
 //
 
 const fs = require('fs');
@@ -15,13 +16,15 @@ const fg = require('fast-glob');
 let appPath = '/Applications/Max.app';
 let shaderPath = '/**/shaders/**/*.jxs';
 let pixPath = '/**/jitter-examples/**/*.genjit';
+let passPath = '/**/passes/**/*.jxp';
 
 if (process.argv.length > 2){
 	appPath = process.argv[2];
 }
 
-shaderMappings(appPath + shaderPath);
-pixMappings(appPath + pixPath);
+// shaderMappings(appPath + shaderPath);
+// pixMappings(appPath + pixPath);
+passMappings(appPath + passPath);
 
 function shaderMappings(p){
 	let mappings = '';
@@ -67,4 +70,20 @@ function pixMappings(p){
 		mappings += `max definesubstitution ${n} ${o} @gen ${b};\n\n`;
 	});
 	fs.writeFileSync('../init/pix-objectmappings.txt', mappings);
+}
+
+function passMappings(p){
+	let mappings = '';
+
+	fg.sync(p).forEach((f) => {
+		let file = path.parse(f);
+		let b = file.base;
+		let n = file.name;
+		let t = n.split('.')[0];
+		let o = 'jit.gl.pass.' + n;
+
+		mappings += `max objectfile ${o} ${o};\n`;
+		mappings += `max definesubstitution ${o} jit.gl.pass @fxname ${n};\n\n`;
+	});
+	fs.writeFileSync('../init/pass-objectmappings.txt', mappings);
 }
